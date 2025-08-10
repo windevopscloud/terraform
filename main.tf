8d32f7ce1b32dce1f59e70e887ec5dfddac3742b
windevopscloud <windevopscloud@gmail.com>
2025-08-10 22:38:26 +0400

    new

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
 # Customize with your own S3 bucket and DynamoDB table if you want to use a Remote Backend for State
 terraform {
   backend "s3" {
-    bucket         = "windevopscloud-terraform-poc-bucket"     # Update it 
+    bucket         = "windevopscloud-terraform-poc-bucket"  # Update it 
     key            = "windevopscloud-terraform-poc.tfstate" # Update it
     region         = "us-east-1"                            # Update it
-    dynamodb_table = "windevopscloud-terraform-poc-lock"                       # Update it
+    dynamodb_table = "windevopscloud-terraform-poc-lock"    # Update it
     encrypt        = true
   }
 }
