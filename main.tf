1facf100915e0ea9676f0fc180db083515a0bece
windevopscloud <windevopscloud@gmail.com>
2025-08-10 22:25:05 +0400

    new

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.0.0"
 }
 
 # Customize with your own S3 bucket and DynamoDB table if you want to use a Remote Backend for State
