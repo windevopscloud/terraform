394207518b7f5a0e6dfdef353573f127df13ccad
windevopscloud <windevopscloud@gmail.com>
2025-08-11 22:58:48 +0400

    multi folder

--- s3-static-website/s3-website.tf
+++ s3-static-website/s3-website.tf
@@ -1,4 +1,5 @@
 #tfsec:ignore:aws-s3-enable-bucket-logging
+#tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket" "static_website" {
   bucket = "windevopscloud-s3-website"
 
@@ -14,8 +15,7 @@ resource "aws_s3_bucket" "static_website" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
-        kms_master_key_id = aws_kms_key.s3_key.arn
+        sse_algorithm = "AES256"
       }
     }
   }
@@ -27,13 +27,14 @@ resource "aws_s3_bucket" "static_website" {
 }
 
 #tfsec:ignore:aws-s3-block-public-policy
+#tfsec:ignore:aws-s3-no-public-buckets
 resource "aws_s3_bucket_public_access_block" "block" {
   bucket = aws_s3_bucket.static_website.id
 
   block_public_acls       = true
   block_public_policy     = false
   ignore_public_acls      = true
-  restrict_public_buckets = true
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "policy" {
@@ -57,4 +58,14 @@ resource "aws_s3_bucket_object" "website_files" {
   bucket = aws_s3_bucket.static_website.bucket
   key    = each.value
   source = "s3-website/${each.value}"
+  content_type = lookup({
+    "html" = "text/html",
+    "css"  = "text/css",
+    "js"   = "application/javascript",
+    "png"  = "image/png",
+    "jpg"  = "image/jpeg",
+    "jpeg" = "image/jpeg",
+    "svg"  = "image/svg+xml",
+    "json" = "application/json",
+  }, split(".", each.value)[length(split(".", each.value)) - 1], "application/octet-stream")
 }
